73b37c70176da957c1f33e51c8eb4457b307d60e
xNombre <kartapolska@gmail.com>
2019-09-20 13:54:08 +0200

    Remove old BT stack leftovers
    Change-Id: I478506e0e70e312c41fe15bbb917e8ced671bb61
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -85,11 +85,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
