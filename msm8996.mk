19373379369a4ea249fe614506baf70c228b8d75
xNombre <kartapolska@gmail.com>
2019-10-23 21:24:23 +0200

    Add ANT+
    Change-Id: Ic886700ab9be73c0a86b9965aa17919d1548f70e
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -399,6 +399,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl
